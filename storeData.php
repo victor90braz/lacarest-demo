136b7a953ff9a90c17ba5377c177823613f08cd8
victor braz <victor.90braz@gmail.com>
2023-09-10 22:04:00 +0200

    refactor storeData

--- storeData.php
+++ storeData.php
@@ -4,8 +4,8 @@ $storeData = [
     "categories" => [
         "electronics" => "Electronics",
         "clothing" => "Clothing",
-        "home_appliances" => "Home Appliances",
-        "sports_outdoors" => "Sports & Outdoors",
+        "homeAppliances" => "Home Appliances",
+        "sportsOutdoors" => "Sports & Outdoors",
     ],
     "products" => [
         [
@@ -32,14 +32,14 @@ $storeData = [
         [
             "id" => 4,
             "name" => "Refrigerator",
-            "category" => "home_appliances",
+            "category" => "homeAppliances",
             "price" => 799.99,
             "description" => "Large capacity refrigerator for your kitchen.",
         ],
         [
             "id" => 5,
             "name" => "Running Shoes",
-            "category" => "sports_outdoors",
+            "category" => "sportsOutdoors",
             "price" => 59.99,
             "description" => "High-quality running shoes for athletes.",
         ],
@@ -49,7 +49,7 @@ $storeData = [
             "id" => 1,
             "username" => "user123",
             "email" => "user123@example.com",
-            "password" => "hashed_password_here", // Password should be hashed in a real system
+            "password" => "hashedPasswordHere", // Password should be hashed in a real system
             "name" => "John Doe",
             "address" => "123 Main St",
             "city" => "Anytown",
@@ -59,7 +59,7 @@ $storeData = [
             "id" => 2,
             "username" => "jane_doe",
             "email" => "jane_doe@example.com",
-            "password" => "hashed_password_here",
+            "password" => "hashedPasswordHere",
             "name" => "Jane Doe",
             "address" => "456 Elm St",
             "city" => "Another City",
@@ -68,24 +68,24 @@ $storeData = [
     ],
     "orderData" => [
         [
-            "order_id" => 101,
-            "user_id" => 1,
+            "orderId" => 101,
+            "userId" => 1,
             "products" => [
-                ["product_id" => 1, "quantity" => 2],
-                ["product_id" => 3, "quantity" => 1],
+                ["productId" => 1, "quantity" => 2],
+                ["productId" => 3, "quantity" => 1],
             ],
-            "total_price" => 539.97,
-            "order_date" => "2023-09-10",
+            "totalPrice" => 539.97,
+            "orderDate" => "2023-09-10",
         ],
         [
-            "order_id" => 102,
-            "user_id" => 2,
+            "orderId" => 102,
+            "userId" => 2,
             "products" => [
-                ["product_id" => 2, "quantity" => 1],
-                ["product_id" => 4, "quantity" => 1],
+                ["productId" => 2, "quantity" => 1],
+                ["productId" => 4, "quantity" => 1],
             ],
-            "total_price" => 1699.98,
-            "order_date" => "2023-09-11",
+            "totalPrice" => 1699.98,
+            "orderDate" => "2023-09-11",
         ],
     ]
 ];
