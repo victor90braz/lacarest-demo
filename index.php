0e0ca340f52ecd07f1e44886964e9311d6d3ae61
victor braz <victor.90braz@gmail.com>
2023-09-10 13:59:38 +0200

    added style

--- index.php
+++ index.php
@@ -5,14 +5,48 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Demo</title>
+    <style>
+        *{
+            box-sizing: border-box;
+            margin: 0;
+
+        }
+
+        body{
+            font-family: 'Roboto', sans-serif;
+        }
+
+        .container{
+            border: 1px solid black;
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        <?php
-            $tittle = "hello world php";
-            echo $tittle;
-        ?>
-    </h1>
+    <div class="container">
+        <header>
+            <h1>
+                <?php
+                $logo = "logo ";
+                $tittle = "brand";
+
+                echo $logo;
+                echo $tittle;
+                ?>
+            </h1>
+
+            <h2>
+                <?php
+                $subTittle = "categories";
+                echo $subTittle;
+                ?>
+            </h2>
+        </header>
+    </div>
 </body>
 </html>
