18a20049360c1d99e3094a6a1689b9032fdf4c6b
victor braz <victor.90braz@gmail.com>
2023-09-11 15:18:27 +0200

    refactor - separate php logic from body html

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php
+$storeData = include 'storeData.php';
+$pathImages = [
+    "menu" => "public/images/menu.png",
+    "logo" => "public/images/logo.png",
+    "info" => "public/images/info.png",
+];
+$menu = "menu";
+$title = "privalia shop";
+$aboutUs = "about us";
+$register = "register";
+$logged = false;
+$greeting = !$logged ? "Please $register" : "Welcome to $title";
+
+function filterData($dataArray, $filterKey, $filterValue) {
+    $filteredData = [];
+
+    foreach ($dataArray as $item) {
+        if (isset($item[$filterKey]) && $item[$filterKey] === $filterValue) {
+            $filteredData[] = $item;
+        }
+    }
+
+    return $filteredData;
+}
+
+function filterProductsByPrice($products, $key) {
+    return array_filter($products, function ($product) use ($key) {
+        return ($product[$key] < 25) || ($product[$key] > 60);
+    });
+}
+
+$filteredUsers = filterData($storeData["users"], "username", "user123");
+$filteredPrices = filterProductsByPrice($storeData["products"], "price");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,43 +47,6 @@
     <title>Privalia Shop</title>
 </head>
 <body>
-
-<?php
-    $storeData = include 'storeData.php';
-    $pathImages = [
-        "menu" => "public/images/menu.png",
-        "logo" => "public/images/logo.png",
-        "info" => "public/images/info.png",
-    ];
-    $menu = "menu";
-    $title = "privalia shop";
-    $aboutUs = "about us";
-    $register = "register";
-    $logged = false;
-    $greeting = !$logged ? "Please $register" : "Welcome to $title";
-
-    function filterData($dataArray, $filterKey, $filterValue) {
-        $filteredData = [];
-
-        foreach ($dataArray as $item) {
-            if (isset($item[$filterKey]) && $item[$filterKey] === $filterValue) {
-                $filteredData[] = $item;
-            }
-        }
-
-        return $filteredData;
-    }
-
-    function filterProductsByPrice($products, $key) {
-        return array_filter($products, function ($product) use ($key) {
-            return ($product[$key] < 25) || ($product[$key] > 60);
-        });
-    }
-
-    $filteredUsers = filterData($storeData["users"], "username", "user123");
-    $filteredPrices = filterProductsByPrice($storeData["products"], "price");
-?>
-
 <div class="container">
     <header class="container-header">
         <menu class="container-menu">
